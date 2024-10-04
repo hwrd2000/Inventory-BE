b2cbdcc6a40832a2953c43e9adf82082ed4d57c2
hwrd2000 <hontiveros.howard@gmail.com>
2024-10-04 18:58:59 +0800

    refactor: Admin Contents

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::prefix('supplier')->middleware(['auth', 'role:supplier'])->group(function () {
-    Filament::routes();
-});
